698ee4aa83f9edb87ebbc168bf47fdb9dea0c986
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-01-28 23:44:52 +0800

    增加战机射击功能

--- pymdl2001/demo/__init__.py
+++ pymdl2001/demo/__init__.py
@@ -3,14 +3,51 @@ from sys import exit
 from constants import *
 
 
+class Bullet(pygame.sprite.Sprite):
+    """
+    子弹类
+    """
+
+    def __init__(self, surface, position):
+        super().__init__()
+        self.image = surface
+        self.rect = self.image.get_rect()
+        self.rect.left = position[0] - self.rect.width / 2 + 2
+        self.rect.top = position[1] - self.rect.height
+        self.speed = 8
+        self.directions = Symbol.DIRECTIONS
+        self.offset = {
+            self.directions[0]: 0,
+            self.directions[1]: 0,
+            self.directions[2]: 0,
+            self.directions[3]: 0
+        }
+
+    def update(self):
+        self.rect.top += (self.offset[K_DOWN] - self.offset[K_UP]) * self.speed
+        if self.rect.top < -self.rect.height:
+            self.kill()
+        elif self.rect.top > WindowGame.SCREEN_HEIGHT:
+            self.kill()
+        self.rect.left += (self.offset[K_RIGHT] - self.offset[K_LEFT]) * self.speed
+        if self.rect.left < -self.rect.width:
+            self.kill()
+        elif self.rect.left > WindowGame.SCREEN_WIDTH:
+            self.kill()
+
+
 class Player(pygame.sprite.Sprite):
+    """
+    玩家飞机类
+    """
+
     def __init__(self, surface, position):
         """
-        玩家飞机类
+        初始化
         :param surface: 飞机图片
         :param position: 飞机初始位置
         """
-        pygame.sprite.Sprite.__init__(self)
+        super().__init__()
         self.image = surface
         self.rect = self.image[0].get_rect()
         self.rect.left = position[0]
@@ -23,6 +60,7 @@ class Player(pygame.sprite.Sprite):
             self.directions[2]: 0,
             self.directions[3]: 0
         }
+        self.bullet = pygame.sprite.Group()
 
     def move(self):
         """
@@ -40,6 +78,11 @@ class Player(pygame.sprite.Sprite):
         elif self.rect.left > WindowGame.SCREEN_WIDTH - self.rect.width:
             self.rect.left = WindowGame.SCREEN_WIDTH - self.rect.width
 
+    def single_shoot(self, bullet_img):
+        bullet = Bullet(bullet_img, self.rect.midtop)
+        bullet.offset[K_UP] = 1
+        self.bullet.add(bullet)
+
 
 def start():
     # 初始化
@@ -53,15 +96,21 @@ def start():
     plane_imgs = [
         shoot_img.subsurface(pygame.Rect(0, 99, 102, 126)),
         shoot_img.subsurface(pygame.Rect(165, 360, 102, 126))]
+    # 子弹图片
+    bullet_img = shoot_img.subsurface(pygame.Rect(1004, 987, 9, 21))
     hero_pos = [185, 500]
     player = Player(plane_imgs, hero_pos)
     directions = Symbol.DIRECTIONS
     while True:
-        pygame.time.Clock().tick(150)
+        pygame.time.Clock().tick(128)
         # 绘制背景
         screen.blit(background, (0, 0))
         # 绘制飞机
         screen.blit(player.image[0 if pygame.time.get_ticks() % 512 > 256 else 1], player.rect)
+        # 绘制子弹
+        if time.get_ticks() % 8 == 0:
+            player.single_shoot(bullet_img)
+        player.bullet.draw(screen)
         # 更新屏幕
         pygame.display.update()
         for event in pygame.event.get():
@@ -80,3 +129,4 @@ def start():
                 if key_up in directions:
                     player.offset[key_up] = 0
         player.move()
+        player.bullet.update()
