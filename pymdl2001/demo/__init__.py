bb1a525d4485fa280fe6565b27aff88739ba8de7
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-02-02 01:53:57 +0800

    加载敌机，添加敌我集中事件处理及记分，调整监测退出及暂停的代码结构，历史最高分记录以及游戏结束结果展示

--- pymdl2001/demo/__init__.py
+++ pymdl2001/demo/__init__.py
@@ -1,6 +1,33 @@
 import pygame
 from sys import exit
 from constants import *
+from random import randint
+
+score_history = 0
+score = 0
+pause = 2
+
+
+class Enemy(pygame.sprite.Sprite):
+    """
+    敌人类
+    """
+
+    def __init__(self, surface, position):
+        super().__init__()
+        self.image = surface
+        self.rect = self.image.get_rect()
+        self.rect.left = position[0]
+        self.rect.top = position[1]
+        self.speed = 6
+        self.countdown = 0
+
+    def update(self):
+        self.rect.top += self.speed
+        if self.rect.top > WindowGame.SCREEN_HEIGHT:
+            global score
+            self.kill()
+            score -= 1  # 错过敌机，扣除一个分数
 
 
 class Bullet(pygame.sprite.Sprite):
@@ -14,7 +41,7 @@ class Bullet(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.left = position[0] - self.rect.width / 2 + 2
         self.rect.top = position[1] - self.rect.height
-        self.speed = 8
+        self.speed = 1
         self.directions = Symbol.DIRECTIONS
         self.offset = {
             self.directions[0]: 0,
@@ -61,12 +88,15 @@ class Player(pygame.sprite.Sprite):
             self.directions[3]: 0
         }
         self.bullet = pygame.sprite.Group()
+        self.is_hit = False
 
     def move(self):
         """
         移动飞机
         :return:
         """
+        if self.is_hit:  # 被击中，失去移动能力
+            return
         self.rect.top += (self.offset[K_DOWN] - self.offset[K_UP]) * self.speed
         if self.rect.top < 200:
             self.rect.top = 200
@@ -79,9 +109,31 @@ class Player(pygame.sprite.Sprite):
             self.rect.left = WindowGame.SCREEN_WIDTH - self.rect.width
 
     def single_shoot(self, bullet_img):
+        if self.is_hit:  # 被击中，失去射击能力
+            return
         bullet = Bullet(bullet_img, self.rect.midtop)
-        bullet.offset[K_UP] = 1
+        bullet.offset[K_UP] = 8
         self.bullet.add(bullet)
+
+
+def check_whitespace_quit(event):
+    """
+    检测暂停或退出事件
+    :param event: pygame.event事件
+    :return: 0,正常进行;1,暂停
+    """
+    global pause
+    if event.type == QUIT:  # 退出
+        quit()
+        exit()
+    elif event.type == KEYDOWN:
+        key_down = event.key
+        if key_down == K_ESCAPE:  # 退出
+            quit()
+            exit()
+        elif key_down == K_SPACE:
+            pause ^= 1
+    return pause
 
 
 def start():
@@ -91,42 +143,111 @@ def start():
     pygame.display.set_caption(WindowGame.CAPTION)
     # 载入图片
     background = pygame.image.load('resources/background.png')
+    gameover = pygame.image.load('resources/gameover.png')
     shoot_img = pygame.image.load('resources/shoot.png')
     # 剪切图片
     plane_imgs = [
         shoot_img.subsurface(pygame.Rect(0, 99, 102, 126)),
-        shoot_img.subsurface(pygame.Rect(165, 360, 102, 126))]
+        shoot_img.subsurface(pygame.Rect(165, 360, 102, 126)),
+        shoot_img.subsurface(pygame.Rect(165, 234, 102, 126)),
+        shoot_img.subsurface(pygame.Rect(330, 624, 102, 126)),
+        shoot_img.subsurface(pygame.Rect(330, 498, 102, 126)),
+        shoot_img.subsurface(pygame.Rect(432, 624, 102, 126)),
+    ]
     # 子弹图片
     bullet_img = shoot_img.subsurface(pygame.Rect(1004, 987, 9, 21))
+    # 敌机图片
+    enemy_img = shoot_img.subsurface(pygame.Rect(534, 612, 57, 43))
+    enemy_destory_img = [
+        shoot_img.subsurface(pygame.Rect(267, 347, 57, 43)),
+        shoot_img.subsurface(pygame.Rect(873, 697, 57, 43)),
+        shoot_img.subsurface(pygame.Rect(267, 296, 57, 43)),
+        shoot_img.subsurface(pygame.Rect(930, 697, 57, 43))
+    ]
     hero_pos = [185, 500]
-    player = Player(plane_imgs, hero_pos)
     directions = Symbol.DIRECTIONS
-    while True:
-        pygame.time.Clock().tick(128)
-        # 绘制背景
-        screen.blit(background, (0, 0))
-        # 绘制飞机
-        screen.blit(player.image[0 if pygame.time.get_ticks() % 512 > 256 else 1], player.rect)
-        # 绘制子弹
-        if time.get_ticks() % 8 == 0:
-            player.single_shoot(bullet_img)
-        player.bullet.draw(screen)
-        # 更新屏幕
+    global score
+    global pause
+    while True:  # 新一局游戏
+        player = Player(plane_imgs, hero_pos)
+        enemy = pygame.sprite.Group()
+        enemy_destory = pygame.sprite.Group()
+        player_countdown = 1
+        score = 0
+        while True:
+            pygame.time.Clock().tick(128)
+            for event in pygame.event.get():
+                if check_whitespace_quit(event) == 1:
+                    break
+                if event.type == KEYDOWN:
+                    key_down = event.key
+                    if key_down in directions:
+                        player.offset[key_down] = 1
+                elif event.type == KEYUP:
+                    key_up = event.key
+                    if key_up in directions:
+                        player.offset[key_up] = 0
+            player.move()
+            if pause == 1:
+                continue
+            # 绘制背景
+            screen.blit(background, (0, 0))
+            # 绘制飞机
+            if player.is_hit:  # 飞机被击中
+                if time.get_ticks() % 24 == 0:  # 开始爆炸动画
+                    player_countdown += 1
+                if player_countdown > 5:  # 爆炸画面播放完成，本轮游戏结束
+                    pause = 1
+                    break
+                screen.blit(player.image[player_countdown], player.rect)
+            else:
+                screen.blit(player.image[0 if pygame.time.get_ticks() % 512 > 256 else 1], player.rect)
+            # 绘制子弹
+            if time.get_ticks() % 8 == 0:
+                player.single_shoot(bullet_img)
+            player.bullet.draw(screen)
+            # 绘制敌机
+            if time.get_ticks() % 32 == 0:
+                enemy.add(Enemy(enemy_img,
+                                (randint(0, WindowGame.SCREEN_WIDTH - enemy_img.get_width()), -enemy_img.get_height())))
+            enemy.update()
+            enemy.draw(screen)
+            # 子弹与敌机碰撞
+            enemy_destory.add(pygame.sprite.groupcollide(enemy, player.bullet, True, True))
+            # 飞机坠落切换动画
+            for e_dest in enemy_destory:
+                screen.blit(enemy_destory_img[e_dest.countdown], e_dest.rect)
+                if time.get_ticks() % 14 == 0:
+                    if e_dest.countdown < 3:
+                        e_dest.countdown += 1
+                    else:
+                        enemy_destory.remove(e_dest)
+                        score += 1  # 击毁敌机，分数+1
+            # 战机被撞
+            player_destorys = pygame.sprite.spritecollide(player, enemy, True)
+            if len(player_destorys) > 0:
+                enemy_destory.add(player_destorys)
+                player.is_hit = True
+            # 显示分数
+            if score < 0:
+                score = 0
+            screen.blit(pygame.font.SysFont("微软雅黑.ttf", 40).render("Score: " + str(score), 3, (255, 0, 0)), (30, 20))
+            # 更新屏幕
+            player.bullet.update()
+            pygame.display.update()
+            if pause == 2:
+                pause = 1
+        # 游戏结束画面
+        global score_history
+        if score < 0:
+            score = 0
+        screen.blit(gameover, (0, 0))
+        screen.blit(pygame.font.SysFont("微软雅黑.ttf", 40).render("Best Score: " + str(score_history), 3, (50, 50, 50)),
+                    (30, 20))
+        screen.blit(pygame.font.SysFont("微软雅黑.ttf", 40).render("Your Score: " + str(score), 3, (255, 255, 255)),
+                    (30, 50))
+        if score > score_history:
+            score_history = score
+            screen.blit(pygame.font.SysFont("微软雅黑.ttf", 22).render("Break the record!", 3, (0, 255, 0)), (220, 60))
+            screen.blit(pygame.font.SysFont("微软雅黑.ttf", 70).render("CONGRATULATE!", 3, (0, 255, 0)), (35, 220))
         pygame.display.update()
-        for event in pygame.event.get():
-            if event.type == QUIT:  # 退出
-                quit()
-                exit()
-            elif event.type == KEYDOWN:
-                key_down = event.key
-                if key_down == K_ESCAPE:  # 退出
-                    quit()
-                    exit()
-                elif key_down in directions:
-                    player.offset[key_down] = 1
-            elif event.type == KEYUP:
-                key_up = event.key
-                if key_up in directions:
-                    player.offset[key_up] = 0
-        player.move()
-        player.bullet.update()
